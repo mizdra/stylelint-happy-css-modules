cc08066eec5764e6f137ddbb2dc5bb0da5ca6697
mizdra <pp.mizdra@gmail.com>
2023-03-26 17:40:51 +0900

    fix lint error

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,5 @@ const rulesPlugins = Object.entries(rules).map(([ruleName, rule]) => {
   return createPlugin(`${NAMESPACE}/${ruleName}`, rule);
 });
 
+// eslint-disable-next-line import/no-default-export
 export default rulesPlugins;
--- src/rules/no-unused-selectors.ts
+++ src/rules/no-unused-selectors.ts
@@ -1,11 +1,11 @@
 import stylelint, { utils } from 'stylelint';
-import { isBoolean } from '../util/validateTypes.js';
 import { NAMESPACE } from '../index';
+import { isBoolean } from '../util/validateTypes.js';
 
 const ruleName = `${NAMESPACE}/no-unused-selectors`;
 
 const messages = utils.ruleMessages(ruleName, {
-  unused: (selector) => `\`${selector}\` is defined but not used.`,
+  unused: (selector: string) => `\`${selector}\` is defined but not used.`,
 });
 
 export const noUnusedSelectors: stylelint.Rule<boolean> = (primaryOption, _secondaryOptions, _context) => {
