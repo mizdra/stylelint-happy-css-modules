ab8915778e6cbd876c8662efb6f08e7713960f03
mizdra <pp.mizdra@gmail.com>
2023-03-26 16:17:20 +0900

    setup example

---
+++ example/01-basic/1.css.d.ts
@@ -0,0 +1,23 @@
+declare const styles:
+  & Readonly<{ "basic": string }>
+  & Readonly<{ "cascading": string }>
+  & Readonly<{ "cascading": string }>
+  & Readonly<{ "pseudo_class_1": string }>
+  & Readonly<{ "pseudo_class_2": string }>
+  & Readonly<{ "pseudo_class_3": string }>
+  & Readonly<{ "multiple_selector_1": string }>
+  & Readonly<{ "multiple_selector_2": string }>
+  & Readonly<{ "combinator_1": string }>
+  & Readonly<{ "combinator_2": string }>
+  & Readonly<{ "at_rule": string }>
+  & Readonly<{ "selector_list_1": string }>
+  & Readonly<{ "selector_list_2": string }>
+  & Readonly<{ "local_class_name_1": string }>
+  & Readonly<{ "local_class_name_2": string }>
+  & Readonly<{ "local_class_name_3": string }>
+  & Readonly<{ "local_class_name_4": string }>
+  & Readonly<{ "composes_target": string }>
+  & Readonly<{ "composes": string }>
+;
+export default styles;
+//# sourceMappingURL=./1.css.d.ts.map
---
+++ example/app.ts
@@ -0,0 +1,21 @@
+import styles1 from './01-basic/1.css';
+
+console.log(styles1.basic);
+console.log(styles1.cascading);
+console.log(styles1.cascading);
+console.log(styles1.pseudo_class_1);
+console.log(styles1.pseudo_class_2);
+console.log(styles1.pseudo_class_3);
+console.log(styles1.multiple_selector_1);
+console.log(styles1.multiple_selector_2);
+console.log(styles1.combinator_1);
+console.log(styles1.combinator_2);
+console.log(styles1.at_rule);
+console.log(styles1.selector_list_1);
+console.log(styles1.selector_list_2);
+console.log(styles1.local_class_name_1);
+console.log(styles1.local_class_name_2);
+console.log(styles1.local_class_name_3);
+console.log(styles1.local_class_name_4);
+console.log(styles1.composes_target);
+console.log(styles1.composes);
